8c3e493d95f5b29325679b0dd88460e0750fe6c6
Alexandr Tkach <can4o1992@gmail.com>
2020-10-10 02:10:37 +0300

    feat: towel sort pass all tests

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let total = [];
+
+  if(!matrix){
+    return total;
+  }
+  else{
+    for(let i = 0; i < matrix.length; i++){
+      if(i % 2 == 1){
+        matrix[i] = matrix[i].reverse();
+      }
+      total.push(...matrix[i]);
+    }
+  }
+
+  return total;
 }
